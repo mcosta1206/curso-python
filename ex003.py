09f52acd97613fc16192745ef13907e569a4488d
Marcio <marciodacostaferreirajunior@gmail.com>
2022-05-16 17:54:51 -0300

    Arquivos do curso de python

---
+++ ex003.py
@@ -0,0 +1,4 @@
+n1 = input('Digite um valor')
+n2 = input('Digite outro valor')
+s = n1 + n2 
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, n3))
---
+++ ex005.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um numero'))
+a = n - 1
+s = n + 2
+print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}'.format(n, a, ))
