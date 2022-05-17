bc8472e23e33d8bdeb50edadf310dbcc5a4beb2d
Marcio <marciodacostaferreirajunior@gmail.com>
2022-05-17 20:46:40 -0300

    Adicionando novos exercicios do curso de Python

---
+++ ex006.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O dobro de {} vale {} '.format(n, d))
+print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}. '.format(n, (n*3), n, pow(n, (1/2))))
