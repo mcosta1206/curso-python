2929c6038913d3182a8f1524ad425e68bfbbb24f
Marcio <marciodacostaferreirajunior@gmail.com>
2022-05-25 14:17:42 -0300

    Novos exercicios do Curso

---
+++ ex051.py
@@ -0,0 +1,14 @@
+núm = int(input('Digite um número: '))
+tot = 0
+for c in range(1, núm + 1):
+    if núm % c == 0:
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '.format(c), end= '')
+print('\n\033[m0 número {} foi fivisível {} vezes'.format(núm, tot))
+if tot == 2:
+    print('E por isso ele É PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
---
+++ ex59.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número para calcular seu Fatorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
+print(' x ' if c > 1 else ' = ', end='')
+f *= c
+c -= 1
+print('{}'.format(f))
